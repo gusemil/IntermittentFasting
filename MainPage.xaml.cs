8872fa868250326bc050410beb1ece700a8e2f44
gusemil <gusemil@hotmail.com>
2024-05-17 17:10:59 +0200

    FEAT: Current time test

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,15 +2,28 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+
+            this.Loaded += CurrentTime;
+        }
+
+        private void CurrentTime(object sender, EventArgs e)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                var timer = new System.Threading.Timer(obj =>
+                {
+                    MainThread.InvokeOnMainThreadAsync(() => { TimeNow.Text = DateTime.Now.ToString("T"); });
+                },null, TimeSpan.FromSeconds(1),TimeSpan.FromSeconds(1));
+            });
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
+            /*
             count++;
 
             if (count == 1)
@@ -19,6 +32,7 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+            */
         }
     }
 
