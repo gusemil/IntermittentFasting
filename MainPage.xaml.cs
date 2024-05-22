3cf1e0832c0598d660e9cc9ac5ca4d1e4938412a
gusemil <gusemil@hotmail.com>
2024-05-22 14:02:01 +0300

    FEAT: Start/Reset fast, Display message when to end fasting

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,45 +5,25 @@ namespace IntermittentFasting
     public partial class MainPage : ContentPage
     {
         private const int intermittentFastingPeriod = 57600; //57600 seconds -> 16 hours
-        private int fastTimerElapsed = 0; //in seconds
-        private System.Timers.Timer fastTimer;
+        private DateTime timeWhenFastCanBeBroken;
+        private bool fastInProgress;
 
         public MainPage()
         {
-            //CreateFastTimer();
             InitializeComponent();
 
             this.Loaded += CurrentTime;
+
+            if (!fastInProgress)
+            {
+                FastTimerBtn.Text = "Start fast";
+            } 
+            else
+            {
+                FastTimerBtn.Text = "Click to reset fast";
+            }
         }
 
-        private void CreateFastTimer()
-        {
-            //Create fast timer
-            fastTimer = new System.Timers.Timer();
-            fastTimer.Interval = 1000; // Set the interval to 1 seconds
-            fastTimer.Elapsed += OnTimedEvent;
-            fastTimer.AutoReset = false;
-            fastTimer.Enabled = true;
-            fastTimer.Start();
-            Console.WriteLine("TIMER CREATED");
-        }
-
-        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
-        {
-            fastTimerElapsed++;
-            FastTimeLbl.Text = "FAK";
-            //FastTimeLbl.Text = (intermittentFastingPeriod - fastTimerElapsed).ToString();
-            //Console.WriteLine((intermittentFastingPeriod - fastTimerElapsed).ToString());
-        }
-
-        /*
-        private ElapsedEventHandler OnTimedEvent()
-        {
-            fastTimerElapsed += 1;
-            FastTimeLbl.Text = (intermittentFastingPeriod - fastTimerElapsed).ToString();
-            return new ElapsedEventHandler();
-        }
-        */
 
         private void CurrentTime(object sender, EventArgs e)
         {
@@ -51,63 +31,18 @@ namespace IntermittentFasting
             {
                 var timer = new System.Threading.Timer(obj =>
                 {
-                    MainThread.InvokeOnMainThreadAsync(() => { TimeNow.Text = DateTime.Now.ToString("T"); });
+                    MainThread.InvokeOnMainThreadAsync(() => { TimeNow.Text = "Current Time: " + DateTime.Now.ToString("T"); });
                 }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
             });
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnFastButtonClicked(object sender, EventArgs e)
         {
-            /*
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-            */
+            timeWhenFastCanBeBroken = DateTime.Now.AddSeconds(intermittentFastingPeriod);
+            FastTimeLbl.Text = "Fast can be broken: " + timeWhenFastCanBeBroken.ToString("T");
+            FastTimerBtn.Text = "Click to reset fast";
+            Application.Current.MainPage.DisplayAlert("", "Fast Started! Fast can be broken " + timeWhenFastCanBeBroken.ToString("T"), "Start Fasting!");
         }
-
-        private void OnFastTimerButtonClicked(object sender, EventArgs e)
-        {
-            //this.Loaded += FastTime;
-            FastTimeLbl.Text = "CLICKED!";
-            //this.Loaded += FastTime;
-            //this.Loaded += FastTimeLeft;
-        }
-
-        private void FastTime(object sender, EventArgs e)
-        {
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                var timer = new System.Threading.Timer(obj =>
-                {
-                    MainThread.InvokeOnMainThreadAsync(() => { FastTimeLbl.Text = DateTime.Now.AddSeconds(intermittentFastingPeriod).ToString("T"); });
-                }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
-            });
-        }
-
-        /*
-        private void FastTimeLeft(object sender, EventArgs e)
-        {
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Interval = 1000; // Set the interval to 1 seconds
-            timer.Elapsed += OnTimedEvent();
-            timer.AutoReset = true;
-            timer.Enabled = false;
-
-            /*
-            MainThread.BeginInvokeOnMainThread(() =>
-            {
-                var timer = new System.Threading.Timer(obj =>
-                {
-                    MainThread.InvokeOnMainThreadAsync(() => { FastTimeLbl.Text = (new DateTime().AddSeconds(intermittentFastingPeriod)).ToString("T"); });
-                }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
-            });
-            */
-        //}
 
     }
 
